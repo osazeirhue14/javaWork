50795280549190e7a3a9c8c6bbc80f8c74f1d31e
osazeirhue14 <154705314+osazeirhue14@users.noreply.github.com>
2025-06-28 11:11:59 +0100

    Create Cat.java

---
+++ runtimePolymorphism/Cat.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Cat {
+
+	public void sound() {
+		System.out.println("The cat goes *meow*");
+	}
+	public void lives() {
+		System.out.println("The cat has 9 lives");
+	}
+	public void runs() {
+		System.out.println("The cat is currently running");
+		
+	}
+
+}
