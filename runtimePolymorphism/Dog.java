65136a904859a8980e0bcc1bd2bc6a269532389a
osazeirhue14 <154705314+osazeirhue14@users.noreply.github.com>
2025-06-28 11:11:39 +0100

    Create Dog.java

---
+++ runtimePolymorphism/Dog.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Dog implements Animal {
+	
+	public void sound() {
+		System.out.println("The dog goes *woof*");
+	}
+	public void lives() {
+		System.out.println("The dog has 1 life");
+	}
+	public void runs() {
+		System.out.println("The dog is currently running");
+	}
+}
